e58d4d238b5ab58d43047c4d10e45d005be17ba6
Timothy Liao <s446424@sinyi.com.tw>
2023-03-05 16:54:47 +0800

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, {NextFunction, Request, Response} from 'express';
 import path from 'path';
 import dotenv from "dotenv";
+import cors from 'cors';
 
 import appRoute from "./app/app.routing";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const envPath = path.resolve(__dirname, `./env/${process.env.NODE_ENV}.env`);
 dotenv.config({path: envPath});
