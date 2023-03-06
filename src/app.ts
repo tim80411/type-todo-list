d69b1d2f37e8814682cffad158bd23e8eb0e64d1
Timothy Liao <s446424@sinyi.com.tw>
2023-03-06 23:09:07 +0800

    refactor: modularize express app in app.ts

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+export class App {
+  private app = express();
+
+  constructor() {
+    this.setEnvironment();
+    this.setHelmet();
+    this.setCors();
+    this.registerRoute();
+  }
+
+  public boot(): void {
+    this.app.listen(process.env.PORT, () => console.log(`API Server is running at port ${process.env.PORT}.`))
+  }
+
+  private setHelmet(): void {
+    this.app.use(helmet());
+  }
+
+  private setCors(): void {
+    this.app.use(cors());
+  }
+
+  private setEnvironment(): void {
+    dotenv.config({path: path.resolve(__dirname, `./env/${process.env.NODE_ENV}.env`)});
+  }
+
+  private registerRoute(): void {
+    this.app.get('/', (req, res, next) => res.send('Hello!'));
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,4 @@
-import express, {NextFunction, Request, Response} from 'express';
-import path from 'path';
-import dotenv from "dotenv";
-import cors from 'cors';
-import helmet from 'helmet';
+import {App} from "./app";
 
-const app = express();
-const envPath = path.resolve(__dirname, `./env/${process.env.NODE_ENV}.env`);
-dotenv.config({path: envPath});
-
-import appRoute from "./app/app.routing";
-import {db} from './database';
-
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
-db.connect();
-
-app.get('/', (req, res, next) => {
-  res.send('Hello, world');
-})
-
-app.use(appRoute);
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({msg: err.message || err});
-})
-
-const {PORT} = process.env;
-app.listen(PORT, () => console.log(`http server is running at ${PORT}`))
+const app = new App();
+app.boot();
